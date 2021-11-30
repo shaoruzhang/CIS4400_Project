9432fd4c9d439cba4d232100bd8924f37fcf924f
shaoru.zhang@baruchmail.cuny.edu <shaoru.zhang@baruchmail.cuny.edu>
2021-11-30 16:26:47 +0000

    second commit

--- models/rat_agency_dim.sql
+++ models/rat_agency_dim.sql
@@ -1,2 +1,9 @@
-SELECT DISTINCT agency, agency_name
-FROM `bigquery-public-data.new_york_311.311_service_requests` WHERE complaint_type = 'Rodent'
+SELECT
+{{ dbt_utils.surrogate_key(['agency_name']) }} AS AgencyDimID,
+agency,
+agency_name
+FROM (
+    SELECT DISTINCT agency, agency_name
+    FROM `bigquery-public-data.new_york_311.311_service_requests` 
+    WHERE complaint_type = 'Rodent'
+)
--- models/unsanitary_agency_dim.sql
+++ models/unsanitary_agency_dim.sql
@@ -1,2 +1,9 @@
-SELECT DISTINCT agency, agency_name
-FROM `bigquery-public-data.new_york_311.311_service_requests` WHERE complaint_type IN ('UNSANITARY CONDITION','Dirty Conditions');
+SELECT
+{{ dbt_utils.surrogate_key(['agency_name']) }} AS AgencyDimID,
+agency,
+agency_name
+FROM (
+    SELECT DISTINCT agency, agency_name
+    FROM `bigquery-public-data.new_york_311.311_service_requests` 
+    WHERE complaint_type IN ('UNSANITARY CONDITION','Dirty Conditions')
+)
